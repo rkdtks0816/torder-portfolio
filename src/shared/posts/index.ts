f47882133aba03d784c970bd0c110e0ab975b8fe
송강산 <rkdtks0816@naver.com>
2024-10-16 19:40:34 +0900

    feat: PWA 적용

--- src/shared/posts/index.ts
+++ src/shared/posts/index.ts
@@ -1,14 +1,20 @@
+
 export interface Post {
-  title: string;
-  tag: string;
-  content: string;
+    title: string;
+    tag: string;
+    content: string;
 }
-import post241010_input_output from "./241010_input_output";
-import post241010_int_char_bool_string from "./241010_int_char_bool_string";
-import post241010_ssO_Ong from "./241010_ssO_Ong";
+import post000000_contents from './000000_contents';
+import post241010_input_output from './241010_input_output';
+import post241010_int_char_bool_string from './241010_int_char_bool_string';
+import post241014_queue_priority_queue from './241014_queue_priority_queue';
+import post241014_vector from './241014_vector';
 
 export const posts = [
-  post241010_input_output,
-  post241010_int_char_bool_string,
-  post241010_ssO_Ong,
+    post000000_contents,
+post241010_input_output,
+post241010_int_char_bool_string,
+post241014_queue_priority_queue,
+post241014_vector
 ];
+  
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ssong-page/", // GitHub Pages의 리포지토리 이름으로 설정
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "My Vite React App",
+        short_name: "ViteReact",
+        description: "My Vite-powered React application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // @를 src 폴더로 매핑
