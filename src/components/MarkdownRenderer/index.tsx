abd27657d2b97069ae0bf8dc41de63a4ef460115
rkdtks0816 <rkdtks0816@naver.com>
2024-11-12 17:08:05 +0900

    feat: 아래첨자 윗첨자 추가

--- src/components/MarkdownRenderer/index.tsx
+++ src/components/MarkdownRenderer/index.tsx
@@ -3,36 +3,42 @@ import { StyledMarkdown } from "./StyledMarkdown";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 import rehypeHighlight from "rehype-highlight";
+import rehypeRaw from "rehype-raw";
 
 interface MarkdownRendererProps {
   content: string;
 }
 
 const removeIndentation = (content: string) => {
-  // 모든 라인에서 공백을 유지하기 위해 첫 번째 라인 공백을 기준으로 조정
   const lines = content.split("\n");
   const minIndentation = Math.min(
       ...lines
-          .filter((line) => line.trim().length > 0) // 빈 줄 제외
+          .filter((line) => line.trim().length > 0)
           .map((line) => line.match(/^\s*/)?.[0].length || 0)
   );
 
-  // 최소 공백을 기준으로 모든 줄에서 공백 제거
   return lines
       .map((line) => (line.startsWith(" ") ? line.slice(minIndentation) : line))
       .join("\n");
 };
 
+const convertSubSuper = (content: string) => {
+  let transformedContent = content.replace(/\^([^^]+)\^/g, "<sup>$1</sup>");
+  transformedContent = transformedContent.replace(/~([^~]+)~/g, "<sub>$1</sub>");
+  return transformedContent;
+};
+
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
   const processedContent = removeIndentation(content);
+  const finalContent = convertSubSuper(processedContent);
 
   return (
-    <StyledMarkdown
-      remarkPlugins={[remarkGfm, remarkBreaks]}
-      rehypePlugins={[rehypeHighlight]}
-    >
-      {processedContent}
-    </StyledMarkdown>
+      <StyledMarkdown
+          remarkPlugins={[remarkGfm, remarkBreaks]}
+          rehypePlugins={[rehypeHighlight, rehypeRaw]}
+      >
+        {finalContent}
+      </StyledMarkdown>
   );
 };
 
--- src/posts/index.ts
+++ src/posts/index.ts
@@ -6,12 +6,13 @@ export interface Post {
 }
 import post1_241106 from './1/241106';
 import post4_241106_1 from './4/241106_1';
+import post4_241112_1 from './4/241112_1';
 
 export const posts = [
     [post1_241106],
 [],
 [],
-[post4_241106_1],
+[post4_241106_1, post4_241112_1],
 [],
 [],
 [],
