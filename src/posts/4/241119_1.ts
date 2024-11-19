280aa326fc7be32cd5f69a8f05181510aca138d9
rkdtks0816 <rkdtks0816@naver.com>
2024-11-19 18:06:19 +0900

    post: 비트와 바이트, 데이터, 명령어

---
+++ src/posts/4/241119_1.ts
@@ -0,0 +1,23 @@
+import { Post } from "../index";
+
+const post: Post = {
+    title: `비트와 바이트`,
+    tag: `#워드`,
+    content: `
+        ## 8비트는 1바이트!
+        - 이전 단위 1000개
+            - kB, MB, GB, TB
+        - 이전 단위 1024개
+            - kiB, MiB, GiB, TiB
+        - 워드
+            > CPU가 한번에 처리할 수 있는 데이터의 크기
+            - 현대 컴퓨터는 32비트 또는 64비트
+        
+        \`\`\`
+        이것이 취업을 위한 컴퓨터 과학이다 
+        with CS 기술 면접을 기반으로 작성한 글입니다.
+        \`\`\`
+    `,
+};
+
+export default post;
--- src/posts/index.ts
+++ src/posts/index.ts
@@ -6,13 +6,15 @@ export interface Post {
 }
 import post1_241106 from './1/241106';
 import post4_241106_1 from './4/241106_1';
-import post4_241112_1 from './4/241112_1';
+import post4_241119_1 from './4/241119_1';
+import post4_241119_2 from './4/241119_2';
+import post4_241119_3 from './4/241119_3';
 
 export const posts = [
     [post1_241106],
 [],
 [],
-[post4_241106_1, post4_241112_1],
+[post4_241106_1, post4_241119_1, post4_241119_2, post4_241119_3],
 [],
 [],
 [],
