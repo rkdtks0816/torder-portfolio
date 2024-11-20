0916d1b51a721855945c02502c0150c4222b0c5d
rkdtks0816 <rkdtks0816@naver.com>
2024-11-20 17:54:56 +0900

    post: 정처기 1-1

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -9,6 +9,13 @@ import post4_241106_1 from './4/241106_1';
 import post4_241119_1 from './4/241119_1';
 import post4_241119_2 from './4/241119_2';
 import post4_241119_3 from './4/241119_3';
+import post10_241120_1 from './10/241120_1';
+import post10_241120_2 from './10/241120_2';
+import post10_241120_3 from './10/241120_3';
+import post10_241120_4 from './10/241120_4';
+import post10_241120_5 from './10/241120_5';
+import post10_241120_6 from './10/241120_6';
+import post10_241120_7 from './10/241120_7';
 
 export const posts = [
     [post1_241106],
@@ -20,7 +27,7 @@ export const posts = [
 [],
 [],
 [],
-[],
+[post10_241120_1, post10_241120_2, post10_241120_3, post10_241120_4, post10_241120_5, post10_241120_6, post10_241120_7],
 [],
 [],
 [],
