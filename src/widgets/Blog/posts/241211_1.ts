0f4ca63c389403e9d02793a5cdac0c25c1584688
rkdtks0816 <rkdtks0816@naver.com>
2024-12-11 20:48:23 +0900

    feat: blogTag 변경

---
+++ src/widgets/Blog/posts/241211_1.ts
@@ -0,0 +1,15 @@
+import { Post } from "@/shared/types/post";
+import { tags } from "../BlogTags";
+
+const post: Post = {
+  title: `이진 탐색`,
+  time: "241211_1",
+  tags: [tags.algorithm, tags.javaScript, tags.binarySearch],
+  content: `
+      \`\`\`
+      
+      \`\`\`
+  `,
+};
+
+export default post;
