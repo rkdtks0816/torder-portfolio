b0d37916f95a16dba92f75e274d0e3d9dadb0066
rkdtks0816 <rkdtks0816@naver.com>
2025-01-08 00:17:24 +0900

    feat: PWA 구현

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public", // PWA 파일이 생성될 폴더
+  register: true,
+  skipWaiting: true,
+});
+const nextConfig: NextConfig = withPWA({
   reactStrictMode: true,
   transpilePackages: ["three"],
-};
+});
 
 export default nextConfig;
